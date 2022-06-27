fc17dd41023902a448d51b5b7999798e24438842
leonardosepulveda <leonardosepulveda@gmail.com>
2022-06-27 15:31:58 -0400

    add useHarmonyPCA argument to allow for previously calculated harmony datasets

--- MERCluster/example_scripts/cluster.py
+++ MERCluster/example_scripts/cluster.py
@@ -19,7 +19,8 @@ def parse_args():
 	parser.add_argument('-preselectedGenesFile', default = 'highVar', type = str, help = 'path to a file containing a list of genes to use for PCA/clustering, expects genes to be entered as one per line with nothing else, set to highVar to select highly variable genes based on dispersion')
 	parser.add_argument('-dispersionMinMaxThreshold', nargs = 3, default = [0.025,4.0,0.5], type = float, metavar = ('min dispersion','max dispersion','dispersion threshold'), help = 'minimum, maximum and cutoff value for selecting highly dispersed genes')
 	parser.add_argument('-regressOut', nargs = '+', default = ['n_counts','percent_mito'], type = str, metavar = 'first variable to regress out', help = 'variables to regress out from counts matrix')
-	parser.add_argument('-usePCA', default = True, type = bool, help = 'whether to perform PCA prior to clustering')
+	parser.add_argument('-usePCA', default = True, type = str, help = 'whether to perform PCA prior to clustering')
+	parser.add_argument('-useHarmonyPCA', default = False, type = str, help = 'to replace the calculated PCAs for the ones precalculated using harmony')
 	parser.add_argument('-kValue', default = 12, type = int, help = 'k value to use for constructing the nearest neighbor graph')
 	parser.add_argument('-resolution', default = [1.0], nargs = '+', type = float, help = 'resolution to use for calculating modularity')
 	parser.add_argument('-clusterMin', default = 10, type = str, help = 'minimum number of cells that must be present in a cluster for it to be included in the final output')
@@ -27,7 +28,8 @@ def parse_args():
 	parser.add_argument('-trackIterations', default = True, type = bool, help = 'whether to track intermediate clustering results')
 	parser.add_argument('-clusteringAlgorithm', default = 'louvain', type = str, help = 'algorithm to use for modularity optimization, louvain or leiden')
 	parser.add_argument('-fileNameIteration', default = 'None', type = str, help = 'variable to allow appending numbers to file names for bootstrapping iterations')
-	parser.add_argument('-processingRecipe', default = 'False', type = str, help = 'flag to designate type of pipeline to use. Options are: merfish, scrnaseq and none')
+	parser.add_argument('-processingRecipe', default = 'None', type = str, help = 'flag to designate type of pipeline to use. Options are: merfish, scrnaseq and none')
+	
 	
 	args = parser.parse_args()
 
@@ -74,6 +76,9 @@ def cluster():
 		
 	if args.usePCA:
 		ex1.selectPCs()
+		if args.useHarmonyPCA:
+			ex1.dataset.obsm['X_pca'] = ex1.dataset.obsm['X_pca_harmony']
+
 	ex1.computeNeighbors(kValue=args.kValue, usePCA=args.usePCA)
 	ex1.cluster(resolution=args.resolution, clusterMin=args.clusterMin, trackIterations=args.trackIterations, clusteringAlgorithm=args.clusteringAlgorithm, preselectedGenesFile = args.preselectedGenesFile)
 
