1a161942f4daa804444aba11b3531602432a3fe8
leonardosepulveda <leonardosepulveda@gmail.com>
2022-07-09 23:03:52 -0400

    add a rewritten version of cluster.py called cluterLS.py

--- MERCluster/example_scripts/cluster.py
+++ MERCluster/example_scripts/cluster.py
@@ -19,19 +19,15 @@ def parse_args():
 	parser.add_argument('-preselectedGenesFile', default = 'highVar', type = str, help = 'path to a file containing a list of genes to use for PCA/clustering, expects genes to be entered as one per line with nothing else, set to highVar to select highly variable genes based on dispersion')
 	parser.add_argument('-dispersionMinMaxThreshold', nargs = 3, default = [0.025,4.0,0.5], type = float, metavar = ('min dispersion','max dispersion','dispersion threshold'), help = 'minimum, maximum and cutoff value for selecting highly dispersed genes')
 	parser.add_argument('-regressOut', nargs = '+', default = ['n_counts','percent_mito'], type = str, metavar = 'first variable to regress out', help = 'variables to regress out from counts matrix')
-	parser.add_argument('-usePCA', default = True, type = str, help = 'whether to perform PCA prior to clustering')
-	parser.add_argument('-useHarmonyPCA', default = False, type = str, help = 'to replace the calculated PCAs for the ones precalculated using harmony')
+	parser.add_argument('-usePCA', default = True, type = bool, help = 'whether to perform PCA prior to clustering')
 	parser.add_argument('-kValue', default = 12, type = int, help = 'k value to use for constructing the nearest neighbor graph')
-	parser.add_argument('-maxValue', default = 4, type = int, help = 'maximum zscore for gene expression')
 	parser.add_argument('-resolution', default = [1.0], nargs = '+', type = float, help = 'resolution to use for calculating modularity')
 	parser.add_argument('-clusterMin', default = 10, type = str, help = 'minimum number of cells that must be present in a cluster for it to be included in the final output')
 	parser.add_argument('-verbose', default = True, type = bool, help = 'whether to print messages during processing')
 	parser.add_argument('-trackIterations', default = True, type = bool, help = 'whether to track intermediate clustering results')
 	parser.add_argument('-clusteringAlgorithm', default = 'louvain', type = str, help = 'algorithm to use for modularity optimization, louvain or leiden')
 	parser.add_argument('-fileNameIteration', default = 'None', type = str, help = 'variable to allow appending numbers to file names for bootstrapping iterations')
-	parser.add_argument('-preprocessing', default = True, type = str, help = 'if preprocessing will be run in the dataset. if true, then preprocessing will be done according to the -merfish option. if false, the user needs to provide an already preprocessed dataset')
 	parser.add_argument('-merfish', default = 'False', type = str, help = 'flag to designate data as coming from a MERFISH experiment, if you set this flag I assume you are giving an h5ad object constructed using area-normalized, logged data')
-	parser.add_argument('-saveAnndata', default = 'False', type = str, help = 'Whether to save the anndata results from clustering.')
 	args = parser.parse_args()
 
 	return args
@@ -40,80 +36,40 @@ def cluster():
 	args = parse_args()
 
 	merfish = args.merfish.upper() == 'TRUE'
-	# preprocessing = args.preprocessing.upper() == 'TRUE'
-	useHarmonyPCA = args.useHarmonyPCA.upper() == 'TRUE'
-	save = args.save.upper() == 'TRUE'
-
 	if args.cellType:
 		ex1 = experiment.Experiment(args.dataFile, args.outputLocation, cellType = args.cellType)
 	else:
 		ex1 = experiment.Experiment(args.dataFile, args.outputLocation)		
 
-	print('input dataset:\n')
-	print(ex1.dataset)
-
-	if args.preprocessing.upper() == 'NONE'
-
-		if merfish:
-
-			#I assume you are giving an h5ad object constructed using area-normalized, logged data
-
-			import scanpy as sc
-
-			if args.pathToCellTypes:
-				ex1.cutToCellList(args.pathToCellTypes, args.pathToCellLabels, args.cellType, args.restriction)
-
-			if args.bootstrapFrac < 1.0:
-				ex1.bootstrapCells(args.fileNameIteration, frac = args.bootstrapFrac)
-
-			sc.pp.scale(ex1.dataset, max_value=args.maxValue)
-		else:
-
-			ex1.filter(verbose = args.verbose, byBatch = args.byBatch, countsPercentileCutoffs = list(args.countsPercentileCutoffs),genesPercentileCutoffs = list(args.genesPercentileCutoffs), mitoPercentileMax = args.mitoPercentileMax, geneMin = args.geneMin)
-
-			if args.pathToCellTypes:
-				ex1.cutToCellList(args.pathToCellTypes, args.pathToCellLabels, args.cellType, args.restriction)
-
-			if args.bootstrapFrac < 1.0:
-				ex1.bootstrapCells(args.fileNameIteration, frac = args.bootstrapFrac)
-
-			ex1.selectVariableGenes(preselectedGenesFile = args.preselectedGenesFile, dispersionMin = args.dispersionMinMaxThreshold[0], dispersionMax = args.dispersionMinMaxThreshold[1], dispersionThreshold = args.dispersionMinMaxThreshold[2])
-			ex1.processData(regressOut=args.regressOut)
-
-	elif args.preprocessing.upper() == 'LOG'
-
-			import scanpy as sc
-
-			if args.pathToCellTypes:
-				ex1.cutToCellList(args.pathToCellTypes, args.pathToCellLabels, args.cellType, args.restriction)
-
-			if args.bootstrapFrac < 1.0:
-				ex1.bootstrapCells(args.fileNameIteration, frac = args.bootstrapFrac)
-
-			sc.pp.scale(ex1.dataset, max_value=args.maxValue)
-
-	elif args.preprocessing.upper() == 'SCALED'
-		print('No preprocessing chosen, data is assumed filtered, logged and then scaled.')
-	else:
-		print(f'preprocessing option \"{args.preprocessing}\" not allowed')
+	if merfish:
+		import scanpy as sc
 
 		if args.pathToCellTypes:
 			ex1.cutToCellList(args.pathToCellTypes, args.pathToCellLabels, args.cellType, args.restriction)
 
 		if args.bootstrapFrac < 1.0:
 			ex1.bootstrapCells(args.fileNameIteration, frac = args.bootstrapFrac)
+
+		sc.pp.scale(ex1.dataset, max_value= 4)
+
+
+	else:
+		ex1.filter(verbose = args.verbose, byBatch = args.byBatch, countsPercentileCutoffs = list(args.countsPercentileCutoffs),genesPercentileCutoffs = list(args.genesPercentileCutoffs), mitoPercentileMax = args.mitoPercentileMax, geneMin = args.geneMin)
+
+		if args.pathToCellTypes:
+			ex1.cutToCellList(args.pathToCellTypes, args.pathToCellLabels, args.cellType, args.restriction)
+
+		if args.bootstrapFrac < 1.0:
+			ex1.bootstrapCells(args.fileNameIteration, frac = args.bootstrapFrac)
+
+		ex1.selectVariableGenes(preselectedGenesFile = args.preselectedGenesFile, dispersionMin = args.dispersionMinMaxThreshold[0], dispersionMax = args.dispersionMinMaxThreshold[1], dispersionThreshold = args.dispersionMinMaxThreshold[2])
+		ex1.processData(regressOut=args.regressOut)
 		
 	if args.usePCA:
 		ex1.selectPCs()
-		if useHarmonyPCA:
-			print('Use harmony')
-			ex1.dataset.obsm['X_pca'] = ex1.dataset.obsm['X_pca_harmony']
-
 	ex1.computeNeighbors(kValue=args.kValue, usePCA=args.usePCA)
 	ex1.cluster(resolution=args.resolution, clusterMin=args.clusterMin, trackIterations=args.trackIterations, clusteringAlgorithm=args.clusteringAlgorithm, preselectedGenesFile = args.preselectedGenesFile)
 
-	if args.saveAnndata:
-		ex1.save(outputFile = f'adata_kValue_{args.kValue}_maxValue_{args.maxValueValue}_resolution_{args.resolution}.h5ad')\
 
 if __name__ == '__main__':
 	cluster()
